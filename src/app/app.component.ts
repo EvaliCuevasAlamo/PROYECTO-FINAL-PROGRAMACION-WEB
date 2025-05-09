32fe555ba89637ff2c1e0df336208a889f560114
OMCM753 <230300753@ucaribe.edu.mx>
2025-05-09 01:58:19 -0500

    Navbar fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatListModule],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'medicalDash';
+
+  
 }
